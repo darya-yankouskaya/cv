2f093ac0653d131bfde72417ab49120df161df98
DaryaYankovskaya <darya.yankovskaya@rockspoon.com>
2023-03-16 18:53:57 +0100

    feat: init introduction component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { IntroductionComponent } from './components/introduction/introduction.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  standalone: true
+  standalone: true,
+  imports: [IntroductionComponent]
 })
 export class AppComponent {}
